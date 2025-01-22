541340838a846685c7e9f67cc4cd272dce590aef
dkvir <kvirika137@gmail.com>
2025-01-22 12:03:49 +0400

    🚀 RELEASE: first commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about-page"></div>
+</template>
+
+<script setup>
+import { useImageStore } from "@/store/imagesLoaded";
+const imageStore = useImageStore();
+
+watch(
+  () => imageStore.isLoaded,
+  (curr) => {
+    console.log(imageStore.loadedImages);
+  }
+);
+
+onMounted(() => {
+  const imageUrls = [];
+
+  for (let i = 0; i < imageStore.totalImages; i++) {
+    imageUrls.push(`https://d3l19dpo35tmza.cloudfront.net/${i + 1}.png`);
+  }
+
+  imageStore.preloadImages(imageUrls);
+});
+</script>
+
+<style lang="scss" scoped></style>
