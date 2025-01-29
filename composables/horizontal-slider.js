21539ba9a86d778e942d6c69d805351ea8cdc866
dkvir <kvirika137@gmail.com>
2025-01-29 16:28:57 +0400

    👌 IMPROVE: four texture atlases

--- composables/horizontal-slider.js
+++ composables/horizontal-slider.js
@@ -1,217 +1,273 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { MapControls } from "three/addons/controls/MapControls.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { SVGLoader } from "three/addons/loaders/SVGLoader.js";
+import { DoubleSide, EquirectangularRefractionMapping } from "three";
+import { useImageStore } from "@/store/imagesLoaded";
 
-export const useHorizontalScroll = class App {
+export const useHorizontalSlider = class App {
   constructor(options) {
     this.options = options;
-    console.log(this.options);
-    // this.createHorizontalScroll();
+    this.imageStore = useImageStore();
+    this.scene = null;
+    this.camera = null;
+    this.orbit = null;
+    this.renderer = null;
+    this.textCanvas = null;
+    this.textCtx = null;
+    this.dummy = null;
+    this.stringBox = null;
+    this.textInputEl = null;
+    this.textureCoordinates = [];
+    this.particles = [];
+    this.stringBox = {
+      wTexture: 0,
+      wScene: 0,
+      hTexture: 0,
+      hScene: 0,
+      caretPosScene: [],
+    };
+    this.string = "1001";
+    this.fontName = "evexweb-Regular";
+    this.textureFontSize = 50;
+    this.fontScaleFactor = 0.15;
+    this.instancedMesh = null;
+
+    this.uvs = [
+      { x: 0.0, y: 0.0, width: 0.0434782608696, height: 0.0434782608696 },
+      {
+        x: 0.0434782608696,
+        y: 0.0,
+        width: 0.0434782608696,
+        height: 0.0434782608696,
+      },
+      // there are much more
+    ];
+    this.uvs2 = [
+      { x: 0.0, y: 0.0, width: 0.0434782608696, height: 0.0434782608696 },
+      {
+        x: 0.0434782608696,
+        y: 0.0,
+        width: 0.0434782608696,
+        height: 0.0434782608696,
+      },
+      //there are much more
+    ];
+
+    this.createScene();
+    this.addEventListeners();
   }
 
-  createHorizontalScroll() {
-    gsap.registerPlugin(ScrollTrigger);
+  createScene() {
+    this.textInputEl = document.querySelector("#text-input");
 
-    let iteration = 0; // gets iterated when we scroll all the way to the end or start and wraps around - allows us to smoothly continue the playhead scrubbing in the correct direction.
+    this.textInputEl.style.fontSize = this.textureFontSize + "px";
+    this.textInputEl.style.font =
+      "100 " + this.textureFontSize + "px " + this.fontName;
+    this.textInputEl.style.lineHeight = 1.1 * this.textureFontSize + "px";
+    this.textInputEl.style.fontWeight = 100;
 
-    // set initial state of items
-    gsap.set(".cards li", { xPercent: 400, opacity: 0, scale: 0 });
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: this.options.canvas,
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.setClearColor(0x000000);
 
-    const spacing = 0.1, // spacing of the cards (stagger)
-      snapTime = gsap.utils.snap(spacing), // we'll use this to snapTime the playhead on the seamlessLoop
-      cards = gsap.utils.toArray(".cards li"),
-      // this function will get called for each element in the buildSeamlessLoop() function, and we just need to return an animation that'll get inserted into a master timeline, spaced
-      animateFunc = (element) => {
-        const tl = gsap.timeline(),
-          bg =
-            element.getAttribute("data-bg") ||
-            gsap.getProperty(element, "backgroundColor");
-        tl.fromTo(
-          element,
-          { scale: 0, opacity: 0 },
-          {
-            scale: 1,
-            opacity: 1,
-            zIndex: 100,
-            backgroundColor: bg,
-            duration: 0.5,
-            yoyo: true,
-            repeat: 1,
-            ease: "power1.in",
-            immediateRender: false,
-          }
-        ).fromTo(
-          element,
-          { xPercent: 400 },
-          { xPercent: -400, duration: 1, ease: "none", immediateRender: false },
-          0
-        );
-        return tl;
-      },
-      seamlessLoop = buildSeamlessLoop(cards, spacing, animateFunc),
-      playhead = { offset: 0 }, // a proxy object we use to simulate the playhead position, but it can go infinitely in either direction and we'll just use an onUpdate to convert it to the corresponding time on the seamlessLoop timeline.
-      wrapTime = gsap.utils.wrap(0, seamlessLoop.duration()), // feed in any offset (time) and it'll return the corresponding wrapped time (a safe value between 0 and the seamlessLoop's duration)
-      scrub = gsap.to(playhead, {
-        // we reuse this tween to smoothly scrub the playhead on the seamlessLoop
-        offset: 0,
-        onUpdate() {
-          seamlessLoop.time(wrapTime(playhead.offset)); // convert the offset to a "safe" corresponding time on the seamlessLoop timeline
-        },
-        duration: 0.5,
-        ease: "power3",
-        paused: true,
-      }),
-      trigger = ScrollTrigger.create({
-        start: 0,
-        onUpdate(self) {
-          let scroll = self.scroll();
-          if (scroll > self.end - 1) {
-            wrap(1, 1);
-          } else if (scroll < 1 && self.direction < 0) {
-            wrap(-1, self.end - 1);
-          } else {
-            scrub.vars.offset =
-              (iteration + self.progress) * seamlessLoop.duration();
-            scrub.invalidate().restart(); // to improve performance, we just invalidate and restart the same tween. No need for overwrites or creating a new tween on each update.
-          }
-        },
-        end: "+=3000",
-        pin: ".gallery",
-      }),
-      // converts a progress value (0-1, but could go outside those bounds when wrapping) into a "safe" scroll value that's at least 1 away from the start or end because we reserve those for sensing when the user scrolls ALL the way up or down, to wrap.
-      progressToScroll = (progress) =>
-        gsap.utils.clamp(
-          1,
-          trigger.end - 1,
-          gsap.utils.wrap(0, 1, progress) * trigger.end
-        ),
-      wrap = (iterationDelta, scrollTo) => {
-        iteration += iterationDelta;
-        trigger.scroll(scrollTo);
-        trigger.update(); // by default, when we trigger.scroll(), it waits 1 tick to update().
-      };
+    this.scene = new THREE.Scene();
 
-    // when the user stops scrolling, snap to the closest item.
-    ScrollTrigger.addEventListener("scrollEnd", () =>
-      scrollToOffset(scrub.vars.offset)
+    // this.camera = new THREE.PerspectiveCamera(
+    //   45,
+    //   window.innerWidth / window.innerHeight,
+    //   0.1,
+    //   1000
+    // );
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      10000
     );
 
-    // feed in an offset (like a time on the seamlessLoop timeline, but it can exceed 0 and duration() in either direction; it'll wrap) and it'll set the scroll position accordingly. That'll call the onUpdate() on the trigger if there's a change.
-    function scrollToOffset(offset) {
-      // moves the scroll playhead to the place that corresponds to the totalTime value of the seamlessLoop, and wraps if necessary.
-      let snappedTime = snapTime(offset),
-        progress =
-          (snappedTime - seamlessLoop.duration() * iteration) /
-          seamlessLoop.duration(),
-        scroll = progressToScroll(progress);
-      if (progress >= 1 || progress < 0) {
-        return wrap(Math.floor(progress), scroll);
-      }
-      trigger.scroll(scroll);
-    }
+    this.camera.position.set(0, 0, 1);
 
-    document
-      .querySelector(".next")
-      .addEventListener("click", () =>
-        scrollToOffset(scrub.vars.offset + spacing)
-      );
-    document
-      .querySelector(".prev")
-      .addEventListener("click", () =>
-        scrollToOffset(scrub.vars.offset - spacing)
-      );
+    this.orbit = new MapControls(this.camera, this.renderer.domElement);
+    this.orbit.enableDamping = true;
 
-    function buildSeamlessLoop(items, spacing, animateFunc) {
-      let rawSequence = gsap.timeline({ paused: true }), // this is where all the "real" animations live
-        seamlessLoop = gsap.timeline({
-          // this merely scrubs the playhead of the rawSequence so that it appears to seamlessly loop
-          paused: true,
-          repeat: -1, // to accommodate infinite scrolling/looping
-          onRepeat() {
-            // works around a super rare edge case bug that's fixed GSAP 3.6.1
-            this._time === this._dur && (this._tTime += this._dur - 0.01);
-          },
-        }),
-        cycleDuration = spacing * items.length,
-        dur; // the duration of just one animateFunc() (we'll populate it in the .forEach() below...
+    // this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
 
-      // loop through 3 times so we can have an extra cycle at the start and end - we'll scrub the playhead only on the 2nd cycle
-      items
-        .concat(items)
-        .concat(items)
-        .forEach((item, i) => {
-          let anim = animateFunc(items[i % items.length]);
-          rawSequence.add(anim, i * spacing);
-          dur || (dur = anim.duration());
-        });
+    this.textCanvas = document.createElement("canvas");
+    this.textCanvas.width = this.textCanvas.height = 0;
+    this.textCtx = this.textCanvas.getContext("2d");
 
-      // animate the playhead linearly from the start of the 2nd cycle to its end (so we'll have one "extra" cycle at the beginning and end)
-      seamlessLoop.fromTo(
-        rawSequence,
-        {
-          time: cycleDuration + dur / 2,
-        },
-        {
-          time: "+=" + cycleDuration,
-          duration: cycleDuration,
-          ease: "none",
-        }
-      );
-      return seamlessLoop;
-    }
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+    this.scene.add(ambientLight);
+    const pointLight = new THREE.PointLight(0xffffff, 1.2);
+    pointLight.position.set(-20, 10, 20);
+    pointLight.castShadow = true;
+    pointLight.shadow.mapSize.width = pointLight.shadow.mapSize.height = 2048;
+    this.scene.add(pointLight);
 
-    // below is the dragging functionality (mobile-friendly too)...
-    Draggable.create(".drag-proxy", {
-      type: "x",
-      trigger: ".cards",
-      onPress() {
-        this.startOffset = scrub.vars.offset;
-      },
-      onDrag() {
-        scrub.vars.offset = this.startOffset + (this.startX - this.x) * 0.001;
-        scrub.invalidate().restart(); // same thing as we do in the ScrollTrigger's onUpdate
-      },
-      onDragEnd() {
-        scrollToOffset(scrub.vars.offset);
-      },
+    const planeGeometry = new THREE.PlaneGeometry(1000, 1000);
+    const trackingPlaneMaterial = new THREE.MeshBasicMaterial({
+      visible: false,
+    });
+    const trackingPlane = new THREE.Mesh(planeGeometry, trackingPlaneMaterial);
+    trackingPlane.position.z = 6;
+    this.scene.add(trackingPlane);
+
+    const shadowPlaneMaterial = new THREE.ShadowMaterial({
+      opacity: 0.2,
     });
 
-    // if you want a more efficient timeline, but it's a bit more complex to follow the code, use this function instead...
-    // function buildSeamlessLoop(items, spacing, animateFunc) {
-    // 	let overlap = Math.ceil(1 / spacing), // number of EXTRA animations on either side of the start/end to accommodate the seamless looping
-    // 		startTime = items.length * spacing + 0.5, // the time on the rawSequence at which we'll start the seamless loop
-    // 		loopTime = (items.length + overlap) * spacing + 1, // the spot at the end where we loop back to the startTime
-    // 		rawSequence = gsap.timeline({paused: true}), // this is where all the "real" animations live
-    // 		seamlessLoop = gsap.timeline({ // this merely scrubs the playhead of the rawSequence so that it appears to seamlessly loop
-    // 			paused: true,
-    // 			repeat: -1, // to accommodate infinite scrolling/looping
-    // 			onRepeat() { // works around a super rare edge case bug that's fixed GSAP 3.6.1
-    // 				this._time === this._dur && (this._tTime += this._dur - 0.01);
-    // 			}
-    // 		}),
-    // 		l = items.length + overlap * 2,
-    // 		time, i, index;
-    //
-    // 	// now loop through and create all the animations in a staggered fashion. Remember, we must create EXTRA animations at the end to accommodate the seamless looping.
-    // 	for (i = 0; i < l; i++) {
-    // 		index = i % items.length;
-    // 		time = i * spacing;
-    // 		rawSequence.add(animateFunc(items[index]), time);
-    // 		i <= items.length && seamlessLoop.add("label" + i, time); // we don't really need these, but if you wanted to jump to key spots using labels, here ya go.
-    // 	}
-    //
-    // 	// here's where we set up the scrubbing of the playhead to make it appear seamless.
-    // 	rawSequence.time(startTime);
-    // 	seamlessLoop.to(rawSequence, {
-    // 		time: loopTime,
-    // 		duration: loopTime - startTime,
-    // 		ease: "none"
-    // 	}).fromTo(rawSequence, {time: overlap * spacing + 1}, {
-    // 		time: startTime,
-    // 		duration: startTime - (overlap * spacing + 1),
-    // 		immediateRender: false,
-    // 		ease: "none"
-    // 	});
-    // 	return seamlessLoop;
-    // }
+    const shadowPlaneMesh = new THREE.Mesh(planeGeometry, shadowPlaneMaterial);
+    shadowPlaneMesh.position.z = -0.2;
+    shadowPlaneMesh.receiveShadow = true;
+    this.scene.add(shadowPlaneMesh);
+
+    this.dummy = new THREE.Object3D();
+
+    this.textInputEl.innerHTML = this.string;
+    this.handleInput();
+    this.refreshText();
+    this.updateParticlesMatrices();
+
+    this.renderer.setAnimationLoop(() => {
+      this.renderer.render(this.scene, this.camera);
+      this.orbit.update();
+    });
+  }
+
+  handleInput() {
+    this.stringBox.wTexture = this.textInputEl.clientWidth;
+    this.stringBox.wScene = this.stringBox.wTexture * this.fontScaleFactor;
+    this.stringBox.hTexture = this.textInputEl.clientHeight;
+    this.stringBox.hScene = this.stringBox.hTexture * this.fontScaleFactor;
+    this.stringBox.caretPosScene = getCaretCoordinates().map(
+      (c) => c * this.fontScaleFactor
+    );
+
+    function getCaretCoordinates() {
+      const range = window.getSelection().getRangeAt(0);
+      const rects = range.getClientRects();
+      if (rects[0]) {
+        return [rects[0].left, rects[0].top];
+      } else {
+        document.execCommand("selectAll", false, null);
+        return [0, 0];
+      }
+    }
+  }
+
+  refreshText() {
+    this.sampleCoordinates();
+
+    this.particles = this.textureCoordinates.map((c, cIdx) => {
+      const x = c.x * this.fontScaleFactor;
+      const y = c.y * this.fontScaleFactor;
+      let p = new useParticle([x, y]);
+      return p;
+    });
+
+    this.createInstancedMesh();
+    this.makeTextFitScreen();
+  }
+
+  sampleCoordinates() {
+    this.textCanvas.width = this.stringBox.wTexture;
+    this.textCanvas.height = this.stringBox.hTexture;
+    this.textCtx.font = "100 " + this.textureFontSize + "px " + this.fontName;
+    this.textCtx.fillStyle = "#2a9d8f";
+    this.textCtx.clearRect(0, 0, this.textCanvas.width, this.textCanvas.height);
+
+    this.textCtx.fillText(this.string, 0, 0.8 * this.stringBox.hTexture);
+
+    // Image data to 2d array
+    const imageData = this.textCtx.getImageData(
+      0,
+      0,
+      this.textCanvas.width,
+      this.textCanvas.height
+    );
+    const imageMask = Array.from(
+      Array(this.textCanvas.height),
+      () => new Array(this.textCanvas.width)
+    );
+    for (let i = 0; i < this.textCanvas.height; i++) {
+      for (let j = 0; j < this.textCanvas.width; j++) {
+        imageMask[i][j] =
+          imageData.data[(j + i * this.textCanvas.width) * 4] > 0;
+      }
+    }
+
+    for (let i = 0; i < this.textCanvas.height; i++) {
+      for (let j = 0; j < this.textCanvas.width; j++) {
+        if (imageMask[i][j]) {
+          this.textureCoordinates.push({
+            x: j,
+            y: i,
+            old: false,
+          });
+        }
+      }
+    }
+  }
+
+  createInstancedMesh() {
+    const textureLoader = new THREE.TextureLoader();
+    const textureAtlas = textureLoader.load("/images/atlas_1-min.png");
+    const textureAtlas2 = textureLoader.load("/images/atlas_2-min.png");
+
+    const material = new THREE.MeshBasicMaterial();
+    const geometry = new THREE.PlaneGeometry(0.09, 0.2);
+
+    this.instancedMesh = new THREE.InstancedMesh(
+      geometry,
+      material,
+      this.particles.length
+    );
+
+    this.scene.add(this.instancedMesh);
+
+    this.instancedMesh.position.x = -0.5 * this.stringBox.wScene;
+    this.instancedMesh.position.y = -0.6 * this.stringBox.hScene;
+
+    this.instancedMesh.castShadow = true;
+  }
+
+  updateParticlesMatrices() {
+    let idx = 0;
+    this.particles.forEach((p) => {
+      this.dummy.position.set(p.x, this.stringBox.hScene - p.y, p.z);
+      this.dummy.scale.set(p.scale, p.scale, p.scale);
+      this.dummy.updateMatrix();
+      this.instancedMesh.setMatrixAt(idx, this.dummy.matrix);
+      idx++;
+    });
+    this.instancedMesh.instanceMatrix.needsUpdate = true;
+  }
+
+  makeTextFitScreen() {
+    const fov = this.camera.fov * (Math.PI / 180);
+    const fovH = 2 * Math.atan(Math.tan(fov / 2) * this.camera.aspect);
+    const dx = Math.abs((0.7 * this.stringBox.wScene) / Math.tan(0.5 * fovH));
+    const dy = Math.abs((0.6 * this.stringBox.hScene) / Math.tan(0.5 * fov));
+    const factor = Math.max(dx, dy) / this.camera.position.length();
+    if (factor > 1) {
+      this.camera.position.x *= factor;
+      this.camera.position.y *= factor;
+      this.camera.position.z *= factor;
+    }
+  }
+
+  addEventListeners() {
+    window.addEventListener("resize", () => {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    });
   }
 };
--- composables/particle.js
+++ composables/particle.js
@@ -3,7 +3,7 @@ export const useParticle = class App {
     this.x = x + 0.1 * (Math.random() - 0.5);
     this.y = y + 0.1 * (Math.random() - 0.5);
     // this.z = z + 0.1 * (Math.random() - 0.5);
-    this.z = 1 * (Math.random() - 0.5);
+    this.z = 1.5 * (Math.random() - 0.5);
 
     this.isGrowing = false;
     this.toDelete = false;
