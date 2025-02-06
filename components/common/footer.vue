6bfaf554338fee37112cf5e652baf1677bec520a
dkvir <kvirika137@gmail.com>
2025-02-06 14:12:14 +0400

    :recycle: REFACTORING: header and footer styles

--- components/common/footer.vue
+++ components/common/footer.vue
@@ -1,149 +1,128 @@
 <template>
-	<div class="footer flex">
-		<nuxt-icon
-			:class="[
-				'take-care',
-				{ 'take-care-gray': textureStore.textureIndex == null },
-			]"
-			name="take-care"
-			filled
-		/>
-		<div
-			:class="[
-				'share flex justify-center',
-				{ 'is-invisible': textureStore.textureIndex == null },
-			]"
-		>
-			<span class="share-text"> <a href="#">გააზიარე</a></span>
-			<nuxt-icon name="share-icon" filled />
-		</div>
-		<div
-			:class="[
-				'visit flex',
-				{ 'visit-gray': textureStore.textureIndex == null },
-			]"
-		>
-			<div class="visit-inside flex justify-center">
-				<a href="#"><span class="uppercase">დაჯავშნე</span></a>
-				<nuxt-icon name="visit-arrow" filled />
-			</div>
-			<span class="visit-text"><a href="#">ვიზიტი ექიმთან</a></span>
-		</div>
-	</div>
+  <div
+    :class="[
+      'footer flex-center',
+      { 'is-invisible': textureStore.textureIndex == null },
+    ]"
+  >
+    <nuxt-icon class="take-care" name="take-care" filled />
+    <a href="#" target="_blank" class="share flex-center">
+      <span class="share-text uppercase">გააზიარე</span>
+      <nuxt-icon name="share-icon" filled />
+    </a>
+    <div class="visit flex-center">
+      <div class="visit-inside flex-center">
+        <a href="#" target="_blank" class="visit-link">
+          <span class="uppercase">დაჯავშნე</span>
+        </a>
+        <nuxt-icon name="visit-arrow" filled />
+      </div>
+      <div class="visit-text">ვიზიტი ექიმთან</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-	import { useTextureStore } from "@/store/texture";
-	const textureStore = useTextureStore();
+import { useTextureStore } from "@/store/texture";
+const textureStore = useTextureStore();
 </script>
 
 <style lang="scss" scoped>
-	.footer {
-		position: fixed;
-		bottom: 50px;
-		left: 64px;
-		right: 64px;
-		z-index: 3;
-		font-family: var(--font-ping-regular);
+.footer {
+  position: fixed;
+  bottom: 50px;
+  left: 64px;
+  right: 64px;
+  z-index: 3;
+  font-family: var(--font-ping-regular);
 
-		.take-care {
-			position: relative;
-			margin-right: 553px;
-			padding: 18px 16px;
-			border-radius: 8px;
+  .take-care {
+    position: relative;
+    margin-right: 553px;
+    padding: 18px 16px;
+    border-radius: 8px;
 
-			&::before {
-				position: absolute;
-				content: "";
-				border-radius: 8px;
-				background-color: var(--color-white);
-				opacity: 0.2;
-				top: 0;
-				left: 0;
-				height: 100%;
-				width: 100%;
-				z-index: -1;
-				backdrop-filter: blur(10px);
-			}
+    &::before {
+      position: absolute;
+      content: "";
+      border-radius: 8px;
+      background-color: var(--take-care-bg, var(--color-white));
+      opacity: 0.2;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      z-index: -1;
+      backdrop-filter: blur(10px);
+    }
 
-			&.take-care-gray::before {
-				background-color: var(--color-evex-gray);
-			}
-		}
+    @include parent-state(".is-invisible") {
+      --take-care-bg: var(--color-evex-gray);
+    }
+  }
 
-		.share {
-			align-items: center;
-			gap: 8px;
-			width: 170px;
-			height: 60px;
-			border-radius: 8px;
-			background-color: var(--color-eleonor);
-			color: var(--color-black);
-			font-size: 18px;
-			margin-left: 220px;
+  .share {
+    padding: 20px 16px;
+    border-radius: 8px;
+    background-color: var(--color-eleonor);
+    color: var(--color-black);
+    opacity: var(--share-opacity, 1);
 
-			&.is-invisible {
-				opacity: 0;
-			}
-		}
+    @include parent-state(".is-invisible") {
+      --share-opacity: 0;
+    }
+    .share-text {
+      font-size: 18px;
+      font-family: var(--font-ping-regular);
+      line-height: 1;
+      margin-right: 8px;
+    }
+  }
 
-		.visit {
-			position: relative;
-			width: 380px;
-			height: 60px;
-			font-size: 20px;
-			align-items: center;
-			border-radius: 8px;
-			color: var(--color-white);
-			padding: 5px;
-			padding-right: 20px;
-			margin-left: auto;
+  .visit {
+    position: relative;
+    border-radius: 8px;
+    color: var(--visit-color, var(--color-white));
+    padding: 5px;
+    margin-left: auto;
 
-			&::before {
-				content: "";
-				position: absolute;
-				background-color: var(--color-white);
-				opacity: 0.3;
-				width: 100%;
-				height: 100%;
-				top: 0;
-				left: 0;
-				border-radius: 8px;
-				z-index: -1;
-			}
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: var(--visit-bg, var(--color-white));
+      opacity: 0.3;
+      width: 100%;
+      height: 100%;
+      border-radius: 8px;
+      z-index: -1;
+    }
 
-			&.visit-gray {
-				color: var(--color-evex-gray);
+    @include parent-state(".is-invisible") {
+      --visit-color: var(--color-evex-gray);
+      --visit-bg: var(--color-evex-gray);
+    }
 
-				&::before {
-					background-color: var(--color-evex-gray);
-				}
-			}
-
-			.visit-inside {
-				align-items: center;
-				opacity: 1;
-				width: 170px;
-				height: 50px;
-				background-color: var(--color-white);
-				color: var(--color-black);
-				gap: 3px;
-				border-radius: 8px;
-			}
-
-			.visit-text {
-				margin-left: auto;
-				color: var(--color-white);
-			}
-		}
-
-		a {
-			color: inherit;
-			text-decoration: none;
-		}
-
-		.share-text {
-			font-size: 18px;
-		}
-	}
+    .visit-inside {
+      opacity: 1;
+      background-color: var(--color-white);
+      color: var(--color-black);
+      border-radius: 8px;
+      padding: 14px 21px;
+    }
+    .visit-link {
+      margin-right: 5px;
+      color: var(--color-black);
+      font-size: 20px;
+      font-family: var(--font-ping-regular);
+    }
+    .visit-text {
+      padding: 0 16px;
+      font-size: 20px;
+      font-family: var(--font-ping-regular);
+      color: var(--color-white);
+    }
+  }
+}
 </style>
--- components/common/header.vue
+++ components/common/header.vue
@@ -1,96 +1,95 @@
 <template>
-	<div class="header flex justify-between">
-		<div
-			:class="[
-				'header-left flex',
-				{
-					'has-background': textureStore.textureIndex == null,
-				},
-			]"
-		>
-			<nuxt-icon name="logo" filled />
-		</div>
-		<a
-			href="#"
-			:class="[
-				'header-right-link',
-				{
-					'has-opacity': textureStore.textureIndex !== null,
-				},
-			]"
-		>
-			<div class="header-right flex justify-between">
-				<span class="play-video-text">ნახე ვიდეო</span>
-				<nuxt-icon name="play-button" filled />
-			</div>
-		</a>
-	</div>
+  <div class="header flex justify-between">
+    <div
+      :class="[
+        'header-left flex',
+        {
+          'has-background': textureStore.textureIndex == null,
+        },
+      ]"
+    >
+      <nuxt-icon name="logo" filled />
+    </div>
+    <a
+      href="#"
+      :class="[
+        'header-right-link flex-center',
+        {
+          'has-opacity': textureStore.textureIndex !== null,
+        },
+      ]"
+    >
+      <div class="play-video-text uppercase">ნახე ვიდეო</div>
+      <nuxt-icon name="play-button" filled />
+    </a>
+  </div>
 </template>
 
 <script setup>
-	import { useTextureStore } from "@/store/texture";
-	const textureStore = useTextureStore();
+import { useTextureStore } from "@/store/texture";
+const textureStore = useTextureStore();
 </script>
 
 <style lang="scss" scoped>
-	.header {
-		position: fixed;
-		top: 64px;
-		left: 64px;
-		right: 64px;
-		z-index: 3;
-		pointer-events: none;
+.header {
+  position: fixed;
+  top: 64px;
+  left: 64px;
+  right: 64px;
+  z-index: 3;
+  pointer-events: none;
 
-		.header-left {
-			width: 196px;
-			background-color: transparent;
-			padding: 12px 16px;
-			border-radius: 8px;
-			pointer-events: all;
+  .header-left {
+    background-color: var(--header-left, transparent);
+    padding: 12px 16px;
+    border-radius: 8px;
+    pointer-events: all;
+    &.has-background {
+      --header-left: var(--color-evex-blue);
+    }
+  }
 
-			&.has-background {
-				background-color: var(--color-evex-blue);
-			}
-		}
+  .header-right-link {
+    position: relative;
+    height: 60px;
+    padding: 15px 12px;
+    text-decoration: none;
+    border-radius: 8px;
+    opacity: var(--play-video-opacity, 1);
+    pointer-events: all;
 
-		.header-right-link {
-			display: block;
-			position: relative;
-			text-decoration: none;
-			color: inherit;
-			border-radius: 8px;
-			opacity: var(--play-video-opacity, 1);
-			pointer-events: all;
+    &::before {
+      content: "";
+      position: absolute;
+      background-color: var(--color-evex-gray);
+      opacity: 0.3;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      border-radius: 8px;
+      z-index: -1;
+    }
 
-			&.has-opacity {
-				--play-video-opacity: 0;
-				pointer-events: none;
-			}
-		}
-
-		.header-right {
-			width: 180px;
-			height: 60px;
-			padding: 15px 12px;
-			display: flex;
-			align-items: center;
-			position: relative;
-			border-radius: 8px;
-			color: white;
-			font-size: 20px;
-
-			&::before {
-				content: "";
-				position: absolute;
-				background-color: var(--color-evex-gray);
-				opacity: 0.3;
-				width: 100%;
-				height: 100%;
-				top: 0;
-				left: 0;
-				border-radius: 8px;
-				z-index: -1;
-			}
-		}
-	}
+    &.has-opacity {
+      --play-video-opacity: 0;
+      pointer-events: none;
+    }
+    .play-video-text {
+      line-height: 1;
+      font-size: 21px;
+      font-family: var(--font-ping-regular);
+      white-space: nowrap;
+      color: var(--color-white);
+    }
+    :deep(.nuxt-icon) {
+      margin-left: 10px;
+      display: block;
+      @include size(42px);
+      svg {
+        @include size(100%);
+      }
+    }
+  }
+}
 </style>
