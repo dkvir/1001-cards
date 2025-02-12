e9bbcdd4339b295ea7478a5372c8d2fb890210f2
dkvir <kvirika137@gmail.com>
2025-02-12 16:41:26 +0400

    👌 IMPROVE: default-seo share image

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,18 +11,28 @@
 const config = useRuntimeConfig();
 const route = useRoute();
 
+useHead({
+  titleTemplate: "ევექსი",
+  meta: [
+    {
+      name: "description",
+      content: "1001 მიზეზი, თუ რატომ უნდა იცხოვრო დიდხანს.",
+    },
+  ],
+});
+
 useServerSeoMeta({
-  ogImage: "/images/share_image.png",
+  ogImage: "/images/share-image.png",
   ogTitle: "ევექსი",
   description: "1001 მიზეზი, თუ რატომ უნდა იცხოვრო დიდხანს.",
   ogDescription: "1001 მიზეზი, თუ რატომ უნდა იცხოვრო დიდხანს.",
   ogUrl: config.public.siteUrl + route.fullPath,
-  ogImage: config.public.siteUrl + "/images/share_image.png",
+  ogImage: config.public.siteUrl + "/images/share-image.png",
   twitterCard: "summary_large_image",
   twitterSite: config.public.siteUrl + route.fullPath,
   twitterTitle: "ევექსი",
   twitterDescription: "1001 მიზეზი, თუ რატომ უნდა იცხოვრო დიდხანს.",
-  twitterImage: "/images/share_image.png",
+  twitterImage: "/images/share-image.png",
 });
 </script>
 
