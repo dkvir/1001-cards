3f4db6337b954a1e37114f4093056f8df88174d7
dkvir <kvirika137@gmail.com>
2025-02-12 16:52:18 +0400

    👌 IMPROVE: change image link

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@ useServerSeoMeta({
   description: "1001 მიზეზი, თუ რატომ უნდა იცხოვრო დიდხანს.",
   ogDescription: "1001 მიზეზი, თუ რატომ უნდა იცხოვრო დიდხანს.",
   ogUrl: config.public.siteUrl + route.fullPath,
-  ogImage: "https://horizontal-slider-chi.vercel.app/images/share-image.png",
+  ogImage: "https://horizontal-slider-chi.vercel.app/images/share-image.png", // change this line
   twitterCard: "summary_large_image",
   twitterSite: config.public.siteUrl + route.fullPath,
   twitterTitle: "ევექსი",
