6521855009d68b2938d95e608eb9c67f52085541
dkvir <kvirika137@gmail.com>
2025-02-17 12:13:47 +0400

    👌 IMPROVE: fb share on mobile

--- components/common/tiny/share.vue
+++ components/common/tiny/share.vue
@@ -38,13 +38,7 @@ const clickShare = () => {
     // Try to open Facebook app
     const fbUrl = `fb://share?link=${url}`;
     window.location.href = fbUrl;
-
-    // Fallback to web after a short delay if app doesn't open
-    setTimeout(() => {
-      window.location.href = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
-    }, 2000);
   } else {
-    // Desktop behavior
     const shareUrl = `https://www.facebook.com/login.php?next=https://www.facebook.com/sharer/sharer.php?u=${url}`;
     window.open(shareUrl, "_blank");
   }
