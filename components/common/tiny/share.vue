bc4a8955923ace96edcf58a9bd243778ebb191a4
dkvir <kvirika137@gmail.com>
2025-02-17 16:28:09 +0400

    👌 IMPROVE: share icons

--- components/common/tiny/share.vue
+++ components/common/tiny/share.vue
@@ -1,14 +1,26 @@
 <template>
-  <div
-    class="share flex-center"
-    :class="{
-      'is-active': textureStore.isShareActive,
-      'is-invisible': textureStore.textureIndex == null,
-    }"
-    @click.prevent="clickShare"
-  >
-    <span class="share-text uppercase">გააზიარე</span>
-    <nuxt-icon name="share-icon" class="share-arrow" filled />
+  <div class="share-parent flex-center">
+    <div
+      class="share share-fb flex-center"
+      :class="{
+        'is-invisible': textureStore.textureIndex == null,
+      }"
+      @click.prevent="clickShareFb"
+    >
+      <nuxt-icon name="social-fb" class="fb-icon" filled />
+      <span class="share-text uppercase">გააზიარე</span>
+    </div>
+    <div
+      class="share share-messenger flex-center"
+      :class="{
+        'is-invisible': textureStore.textureIndex == null,
+        'is-hidden': !isMobile,
+      }"
+      @click.prevent="clickShareMessenger"
+    >
+      <nuxt-icon name="social-messenger" class="messenger-icon" filled />
+      <span class="share-text uppercase">გააზიარე</span>
+    </div>
   </div>
 </template>
 
@@ -16,80 +28,97 @@
 import { useTextureStore } from "@/store/texture";
 
 const textureStore = useTextureStore();
+const isMobile = ref(null);
+onMounted(() => {
+  isMobile.value = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+});
 
-const clickShare = () => {
+const clickShareFb = () => {
   textureStore.toggleShare(!textureStore.isShareActive);
 
   const url = encodeURIComponent(window.location.href);
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-  if (isMobile) {
+  if (isMobile.value) {
     const fbUrl = `fb://share?link=${url}`;
     window.location.href = fbUrl;
-
-    // messenger
-    // const url = encodeURIComponent(window.location.href);
-    // window.open(`fb-messenger://share?link=${window.location.href}`, "_blank");
   } else {
     const shareUrl = `https://www.facebook.com/login.php?next=https://www.facebook.com/sharer/sharer.php?u=${url}`;
     window.open(shareUrl, "_blank");
   }
 };
+
+const clickShareMessenger = () => {
+  const url = encodeURIComponent(window.location.href);
+  window.open(`fb-messenger://share?link=${url}`, "_blank");
+};
 </script>
 
 <style lang="scss" scoped>
-.share {
+.share-parent {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate3d(-50%, -50%, 0);
   height: var(--app-header-height);
-  padding: 0 css-clamp(16px, 20px);
-  border-radius: 8px;
-  background-color: var(--share-hover-bg-color, var(--color-eleonor));
-  color: var(--share-hover-color, var(--color-black));
-  opacity: var(--share-opacity, 1);
-  border: 2px solid var(--active-border, transparent);
-  pointer-events: all;
-  cursor: pointer;
-  @include default-transitions(background-color, color, border);
+  .share {
+    height: 100%;
+    padding: 0 css-clamp(16px, 20px);
+    border-radius: 8px;
+    background-color: var(--share-hover-bg-color, var(--color-eleonor));
+    color: var(--share-hover-color, var(--color-black));
+    opacity: var(--share-opacity, 1);
+    border: 2px solid var(--active-border, transparent);
+    pointer-events: all;
+    cursor: pointer;
+    @include default-transitions(background-color, color, border);
 
-  @media (max-width: 540px) {
-    width: css-clamp-vw(287px, 450px, 540);
-  }
-
-  &.is-invisible {
-    --share-opacity: 0;
-    pointer-events: none;
-  }
-
-  &:hover {
-    --share-hover-bg-color: var(--color-evex-blue);
-  }
-
-  &.is-active {
-    --share-hover-bg-color: var(--color-evex-blue);
-    --share-hover-color: var(--color-white);
-    --stroke-hover: var(--color-white);
-    --active-border: var(--color-white);
-  }
-
-  :deep(.share-arrow) {
-    @include size(css-clamp(20px, 24px));
-    svg {
-      @include size(100%);
+    &.share-messenger {
+      margin-left: 12px;
+      &.is-hidden {
+        display: none;
+      }
     }
-    path {
-      stroke: var(--stroke-hover, var(--color-black));
-      @include default-transitions(stroke);
-    }
-  }
 
-  .share-text {
-    font-size: css-clamp(14px, 18px);
-    font-family: var(--font-ping-regular);
-    line-height: 1;
-    margin-right: 8px;
+    @media (max-width: 540px) {
+      width: min-content;
+    }
+
+    &.is-invisible {
+      --share-opacity: 0;
+      pointer-events: none;
+    }
+
+    &:hover {
+      --share-hover-bg-color: var(--color-evex-blue);
+      --fb-icon-fill: var(--color-evex-blue);
+    }
+
+    :deep(.nuxt-icon) {
+      @include size(css-clamp(20px, 24px));
+
+      &.fb-icon {
+        svg {
+          path {
+            fill: var(--fb-icon-fill, var(--color-white));
+            @include default-transitions(fill);
+          }
+        }
+      }
+      svg {
+        @include size(100%);
+      }
+      path {
+        stroke: var(--stroke-hover, var(--color-black));
+        @include default-transitions(stroke);
+      }
+    }
+
+    .share-text {
+      font-size: css-clamp(14px, 18px);
+      font-family: var(--font-ping-regular);
+      line-height: 1;
+      margin-left: 8px;
+    }
   }
 }
 </style>
