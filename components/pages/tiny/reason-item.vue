327434db99281370192ada4bb2408e4cf616790a
dkvir <kvirika137@gmail.com>
2025-03-12 15:00:02 +0400

    👌 IMPROVE: reasons item

---
+++ components/pages/tiny/reason-item.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    :class="[
+      'reason-item flex align-center',
+      { 'first-ten-reason': index <= 10 },
+    ]"
+  >
+    <div class="number flex align-center">
+      <nuxt-icon name="number" class="number-icon" filled />
+      <div class="reason-id">{{ getCardId(item.cardIndex) }}</div>
+    </div>
+    <h2 class="reason uppercase">{{ item.reason }}</h2>
+    <pages-tiny-arrow-right />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  item: {
+    type: Object,
+  },
+  index: {
+    type: Number,
+  },
+});
+
+const getCardId = (cardIndex) => {
+  const idToString = cardIndex.toString();
+
+  if (idToString.length == 1) {
+    return "00" + idToString;
+  } else if (idToString.length == 2) {
+    return "0" + idToString;
+  } else {
+    return idToString;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.reason-item {
+  padding: 70px 0;
+  .number {
+    .reason-id {
+      margin-left: 12px;
+      font-family: var(--font-evex-regular);
+      font-size: 72px;
+      line-height: 1;
+      color: var(--color-evex-green);
+    }
+  }
+  .reason {
+    margin-left: 70px;
+    font-size: 34px;
+    font-family: var(--font-ping-regular);
+  }
+}
+</style>
--- pages/reasons.vue
+++ pages/reasons.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="reasons">
+  <div
+    :class="[
+      'reasons',
+      {
+        'is-loaded': textureloadedStore.loaderComplete,
+      },
+    ]"
+  >
     <nuxt-icon name="1001-reasons" class="reasons-icon flex-center" filled />
     <ul class="list">
-      <li
-        v-for="(item, index) in use1001Copy()"
-        :key="index"
-        class="item flex align-center"
-      >
-        <div class="number flex align-center">
-          <nuxt-icon name="number" class="number-icon" filled />
-          <div class="reason-id">{{ getCardId(item.cardIndex) }}</div>
-        </div>
-        <h2 class="reason uppercase">{{ item.reason }}</h2>
-        <pages-tiny-arrow-right />
+      <li v-for="(item, index) in use1001Copy()" :key="index" class="item">
+        <pages-tiny-reason-item :item="item" :index="index" />
       </li>
     </ul>
   </div>
@@ -20,7 +18,9 @@
 
 <script setup>
 import { usePageLink } from "@/store/page-link";
+import { useTextureLoaderStore } from "@/store/texturesLoaded";
 
+const textureloadedStore = useTextureLoaderStore();
 const pageLink = usePageLink();
 
 onMounted(() => {
@@ -28,23 +28,18 @@ onMounted(() => {
     pageLink.changePageLoading(false);
   }, 500);
 });
-
-const getCardId = (cardIndex) => {
-  const idToString = cardIndex.toString();
-
-  if (idToString.length == 1) {
-    return "00" + idToString;
-  } else if (idToString.length == 2) {
-    return "0" + idToString;
-  } else {
-    return idToString;
-  }
-};
 </script>
 
 <style lang="scss" scoped>
 .reasons {
   padding: var(--page-offset-padding);
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  &.is-loaded {
+    height: 100vh;
+    overflow: auto;
+  }
   :deep(.reasons-icon) {
     padding-top: calc(var(--app-header-height) + 70px);
     svg {
@@ -57,28 +52,13 @@ const getCardId = (cardIndex) => {
   }
 
   .item {
-    padding: 70px 0;
     border-bottom: 1px solid var(--color-border);
     cursor: pointer;
+    overflow: hidden;
     &:hover {
       --arrow-bg: var(--color-evex-green);
       --stroke-color: var(--color-white);
     }
-  }
-
-  .number {
-    .reason-id {
-      margin-left: 12px;
-      font-family: var(--font-evex-regular);
-      font-size: 72px;
-      line-height: 1;
-      color: var(--color-evex-green);
-    }
-  }
-  .reason {
-    margin-left: 70px;
-    font-size: 34px;
-    font-family: var(--font-ping-regular);
   }
 }
 </style>
